7568b7e05c0962ba78987e5005d6c894316c6f9f
Joe-Tschirpig <48662242+Joe-Tschirpig@users.noreply.github.com>
2019-04-12 17:47:49 +0930

    Fix memory leak in editor for StaticMemoryPoolRegistry

--- UnityProject/Assets/Plugins/Zenject/Source/Factories/Pooling/Util/StaticMemoryPoolRegistry.cs
+++ UnityProject/Assets/Plugins/Zenject/Source/Factories/Pooling/Util/StaticMemoryPoolRegistry.cs
@@ -10,23 +10,45 @@ namespace Zenject
         public static event Action<IMemoryPool> PoolAdded = delegate {};
         public static event Action<IMemoryPool> PoolRemoved = delegate {};
 
-        readonly static List<IMemoryPool> _pools = new List<IMemoryPool>();
+        readonly static List<WeakReference<IMemoryPool>> _pools = new List<WeakReference<IMemoryPool>>();
 
-        public static IEnumerable<IMemoryPool> Pools
+        public static IEnumerable<WeakReference<IMemoryPool>> Pools
         {
             get { return _pools; }
         }
 
         public static void Add(IMemoryPool memoryPool)
         {
-            _pools.Add(memoryPool);
+            _pools.Add(new WeakReference<IMemoryPool>(memoryPool));
             PoolAdded(memoryPool);
         }
 
         public static void Remove(IMemoryPool memoryPool)
         {
-            _pools.RemoveWithConfirm(memoryPool);
-            PoolRemoved(memoryPool);
+            int index = FindIndex(memoryPool);
+
+            if (index >= 0)
+            {
+                _pools.RemoveAt(index);
+                PoolRemoved(memoryPool);
+            }
+            else
+            {
+                Assert.CreateException();
+            }
+        }
+
+        private static int FindIndex(IMemoryPool memoryPool)
+        {
+            for (int i = 0; i < _pools.Count; i++)
+            {
+                IMemoryPool pool;
+                if (_pools[i].TryGetTarget(out pool) && pool == memoryPool)
+                {
+                    return i;
+                }
+            }
+            return -1;
         }
     }
 #endif
